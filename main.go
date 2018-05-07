3f9875a1995b7db323bfb359120415d9ee4d23a8
Thomas Desrosiers <github@hive.pw>
2018-05-06 20:48:15 -0700

    Pull out the S3 interactions

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/golang/groupcache"
 	"github.com/pkg/errors"
 )
@@ -23,40 +22,7 @@ var (
 	bucket = flag.String("bucket", "", "Bucket ot use for S3 client")
 )
 
-func s3GetSize(s3c *s3.S3, bucket string, key string) (int64, error) {
-	r, err := s3c.HeadObject(&s3.HeadObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-	})
-	if err != nil {
-		return 0, err
-	}
 
-	if r.ContentLength != nil {
-		return *r.ContentLength, nil
-	}
-	return 0, nil
-}
-
-func s3Get(dl *s3manager.Downloader, bucket string, key string) ([]byte, error) {
-	log.Println("S3 get triggered")
-
-	size, err := s3GetSize(dl.S3.(*s3.S3), bucket, key)
-	if err != nil {
-		return nil, errors.Wrap(err, "Unable to head object")
-	}
-
-	buf := aws.NewWriteAtBuffer(make([]byte, size))
-	dl.Download(buf, &s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-	})
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to download object")
-	}
-
-	return buf.Bytes(), nil
-}
 
 func parseArgs() {
 	flag.Parse()
@@ -76,23 +42,13 @@ func main() {
 			Endpoint:         aws.String("http://localhost:9000"),
 		})),
 	)
-	downloader := s3manager.NewDownloaderWithClient(s3c)
-	uploader := s3manager.NewUploaderWithClient(s3c)
+
+	s3cache := NewS3Cache(s3c, *bucket)
 
 	group := groupcache.NewGroup(
 		"bazel-cache",
 		2<<32,
-		groupcache.GetterFunc(func(ctx groupcache.Context, key string, dest groupcache.Sink) error {
-			b, err := s3Get(downloader, *bucket, key)
-			if err != nil {
-				return errors.Wrap(err, "failed hydration during s3 get")
-			}
-
-			return errors.Wrap(
-				dest.SetBytes(b),
-				"failed hydration when setting bytes on the groupcache sync",
-			)
-		}),
+		s3cache,
 	)
 
 	go func() {
@@ -111,7 +67,6 @@ func main() {
 		switch r.Method {
 		case "GET":
 			var b groupcache.ByteView
-
 			err := group.Get(nil, key, groupcache.ByteViewSink(&b))
 			if err := errors.Cause(err); err != nil {
 				if awsErr, ok := err.(awserr.RequestFailure); ok && awsErr.StatusCode() == http.StatusNotFound {
@@ -127,11 +82,7 @@ func main() {
 				log.Println(errors.Wrap(err, "error sending http get reply"))
 			}
 		case "PUT":
-			_, err := uploader.UploadWithContext(r.Context(), &s3manager.UploadInput{
-				Bucket: bucket,
-				Key:    aws.String(key),
-				Body:   r.Body,
-			})
+			err := s3cache.Put(r.Context(), key, r.Body)
 
 			if err != nil {
 				log.Println(errors.Wrap(err, "http put request failed"))
---
+++ s3.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/golang/groupcache"
+	"github.com/pkg/errors"
+)
+
+// S3Cache manages the relationship between groupcache and S3
+type S3Cache struct {
+	s3c *s3.S3
+	up  *s3manager.Uploader
+	dl  *s3manager.Downloader
+
+	bucket string
+}
+
+func NewS3Cache(s3c *s3.S3, bucket string) *S3Cache {
+	return &S3Cache{
+		s3c: s3c,
+		up:  s3manager.NewUploaderWithClient(s3c),
+		dl:  s3manager.NewDownloaderWithClient(s3c),
+
+		bucket: bucket,
+	}
+}
+
+func bestEffortGetSize(s3c *s3.S3, bucket, key string) int64 {
+	r, _ := s3c.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	if r != nil && r.ContentLength != nil {
+		return *r.ContentLength
+	}
+	return 0
+}
+
+// Get implements the groupcache.Getter interface
+func (s *S3Cache) Get(groupCacheContext groupcache.Context, key string, dest groupcache.Sink) error {
+	log.Printf("Hydration request called, pulling %q from S3", key)
+
+	size := bestEffortGetSize(s.s3c, s.bucket, key)
+	buf := aws.NewWriteAtBuffer(make([]byte, size))
+
+	_, err := s.dl.Download(buf, &s3.GetObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to download object")
+	}
+
+	return errors.Wrap(
+		dest.SetBytes(buf.Bytes()),
+		"failed hydration when setting bytes on the groupcache sync",
+	)
+}
+
+func (s *S3Cache) Put(ctx context.Context, key string, r io.Reader) error {
+	_, err := s.up.UploadWithContext(
+		ctx,
+		&s3manager.UploadInput{
+			Bucket: bucket,
+			Key:    aws.String(key),
+			Body:   r,
+		},
+	)
+
+	return err
+}
